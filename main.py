8fdeef8364be239dcebcdea9cc26ce8a70f3df71
student <aleksandrvg79@gmail.com>
2023-10-10 21:03:42 +0300

    number of non-repeating elements

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from square import circle
+import random
 
 print(circle(10))
+
+array = []
+for i in range(100):
+    a = random.randint(1, 20)
+    array.append(a)
+print(f'Количество {len(array)}: {array}')
+
+mno = set(array)
+array = list(mno)
+print(f'Количество {len(array)}: {array}')
